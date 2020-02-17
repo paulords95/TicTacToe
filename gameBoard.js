0b9aec223f210ff7502f520d1e15284678ff86c9
Paulo Ricardo da Silva <paulo.silva@quimtia.net.br>
2020-02-17 15:26:41 -0300

    created array of choices

--- gameBoard.js
+++ gameBoard.js
@@ -1,5 +1,6 @@
 const gB = document.querySelector('#gameboard');
 const container = document.querySelector('#container');
+let played = [[0],[1],[2]];
 
 function createGame (){
     for (let i = 0; i < 3; i++){
@@ -20,15 +21,25 @@ const gameboard = document.querySelectorAll('.boardSlot');
 
 let lastPlay = '';
 let choice= '';
-let played = [];
 
 for (let m = 0; m < gameboard.length; m++){
     gameboard[m].addEventListener('click', function(){
         if (lastPlay == '' || lastPlay == playerOne.choice){
             choice = playerTwo.choice;
+            if (played[0].length < 4){
+                played[0].push(choice)
+            } else {
+                played[2].push(choice)
+            }
+            
         }
         if (lastPlay == playerTwo.choice){
             choice = playerOne.choice;
+            if (played[1].length < 4){
+                played[1].push(choice)
+            } else {
+                played[2].push(choice)
+            }
         }
         if (gameboard[m].innerHTML == ''){
             gameboard[m].innerHTML = choice;
@@ -38,6 +49,12 @@ for (let m = 0; m < gameboard.length; m++){
         }
     })
 }
+
+
+for (let g = 0; g < gameboard.length; g++){
+   // gameboard[g].innerHTML = g;
+}
+
 
 
 
