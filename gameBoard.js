ac21d51d11d8ebb676dbc8773274ac11e4feacf1
Paulo Ricardo da Silva <paulo.silva@quimtia.net.br>
2020-02-18 11:14:27 -0300

    added status and start button

--- gameBoard.js
+++ gameBoard.js
@@ -1,11 +1,11 @@
 const gB = document.querySelector('#gameboard');
 const container = document.querySelector('#container');
-let played = [[0],[1],[2]];
+const score = document.querySelector('#score');
+const startGame = document.createElement('button');
 
 function createGame (){
     for (let i = 0; i < 3; i++){
-        const fstRow = document.createElement('div');
-        //fstRow.setAttribute('class', 'boardCol'); 
+        const fstRow = document.createElement('div'); 
         for (let l = 0; l < 3; l++){
             const scdRow = document.createElement('div');
             scdRow.setAttribute('class', 'boardSlot');
@@ -17,44 +17,94 @@ function createGame (){
 
 createGame();
 
-const gameboard = document.querySelectorAll('.boardSlot');
 
+startGame.setAttribute('id', 'start');
+startGame.innerHTML = 'START GAME'
+score.appendChild(startGame)
+
+const gameboard = document.querySelectorAll('.boardSlot');
 let lastPlay = '';
 let choice= '';
+function playGame(){
+    for (let m = 0; m < gameboard.length; m++){
+        gameboard[m].addEventListener('click', function(){
+            if (lastPlay == '' || lastPlay == playerOne.choice){
+                choice = playerTwo.choice;    
+            }
+            if (lastPlay == playerTwo.choice){
+                choice = playerOne.choice;
+            }
+            if (gameboard[m].innerHTML == ''){
+                gameboard[m].innerHTML = choice;
+                lastPlay = choice;
+            } 
+            if (status.innerHTML == 'X won!' || 
+                    status.innerHTML == 'O won!' &&
+                    gameboard[m].innerHTML == '') {
+                gameboard[m].innerHTML = 'END';
+            }
 
-for (let m = 0; m < gameboard.length; m++){
-    gameboard[m].addEventListener('click', function(){
-        if (lastPlay == '' || lastPlay == playerOne.choice){
-            choice = playerTwo.choice;
-            if (played[0].length < 4){
-                played[0].push(choice)
-            } else {
-                played[2].push(choice)
-            }
-            
-        }
-        if (lastPlay == playerTwo.choice){
-            choice = playerOne.choice;
-            if (played[1].length < 4){
-                played[1].push(choice)
-            } else {
-                played[2].push(choice)
-            }
-        }
-        if (gameboard[m].innerHTML == ''){
-            gameboard[m].innerHTML = choice;
-            lastPlay = choice;
-        } else {
-            console.log('lugar já preenchido')
-        }
-    })
+        
+            checkWinner (0, 1, 2, 'O');
+            checkWinner (3, 4, 5, 'O');
+            checkWinner (6, 7, 8, 'O');
+            checkWinner (2, 1, 0, 'O');
+            checkWinner (5, 4, 3, 'O');
+            checkWinner (8, 7, 6, 'O');
+            checkWinner (0, 4, 8, 'O');
+            checkWinner (8, 4, 0, 'O');
+            checkWinner (2, 4, 6, 'O');
+            checkWinner (6, 4, 2, 'O');
+            checkWinner (0, 3, 6, 'O');
+            checkWinner (6, 3, 0, 'O');
+            checkWinner (1, 4, 7, 'O');
+            checkWinner (1, 4, 7, 'O');
+            checkWinner (2, 5, 8, 'O');
+            checkWinner (2, 5, 8, 'O');   
+        
+            checkWinner (0, 1, 2, 'X');
+            checkWinner (3, 4, 5, 'X');
+            checkWinner (6, 7, 8, 'X');
+            checkWinner (2, 1, 0, 'X');
+            checkWinner (5, 4, 3, 'X');
+            checkWinner (8, 7, 6, 'X');
+            checkWinner (0, 4, 8, 'X');
+            checkWinner (8, 4, 0, 'X');
+            checkWinner (2, 4, 6, 'X');
+            checkWinner (6, 4, 2, 'X');
+            checkWinner (0, 3, 6, 'X');
+            checkWinner (6, 3, 0, 'X');
+            checkWinner (1, 4, 7, 'X');
+            checkWinner (1, 4, 7, 'X');
+            checkWinner (2, 5, 8, 'X');
+            checkWinner (2, 5, 8, 'X');
+        })
+    } 
+}
+
+const status = document.createElement('div');
+status.setAttribute('id', 'status');
+score.appendChild(status);
+
+
+startGame.addEventListener('click', function(){
+    playGame();
+    startGame.remove();
+    status.innerHTML = 'Playing'
+})
+
+function checkWinner (posOne, posTwo, posThree, choice){
+    if(gameboard[posOne].innerHTML == gameboard[posTwo].innerHTML &&
+        gameboard[posOne].innerHTML == gameboard[posThree].innerHTML &&
+        gameboard[posThree].innerHTML  == choice){
+            status.innerHTML = choice + ' won!'
+        } 
 }
 
 
-for (let g = 0; g < gameboard.length; g++){
-   // gameboard[g].innerHTML = g;
+if (status.innerHTML == 'X won!' || status.innerHTML == 'O won!'){
+   alert('teste')
 }
-
 
 
 
