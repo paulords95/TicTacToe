ea29c3dc672b0c29203c2142cd9dc302103d03d1
Paulo Ricardo da Silva <paulo.silva@quimtia.net.br>
2020-02-19 08:09:15 -0300

    removed unecessary check probabilities

--- gameBoard.js
+++ gameBoard.js
@@ -47,35 +47,19 @@ function playGame(){
             checkWinner (0, 1, 2, 'O');
             checkWinner (3, 4, 5, 'O');
             checkWinner (6, 7, 8, 'O');
-            checkWinner (2, 1, 0, 'O');
-            checkWinner (5, 4, 3, 'O');
-            checkWinner (8, 7, 6, 'O');
             checkWinner (0, 4, 8, 'O');
-            checkWinner (8, 4, 0, 'O');
             checkWinner (2, 4, 6, 'O');
-            checkWinner (6, 4, 2, 'O');
             checkWinner (0, 3, 6, 'O');
-            checkWinner (6, 3, 0, 'O');
-            checkWinner (1, 4, 7, 'O');
             checkWinner (1, 4, 7, 'O');
             checkWinner (2, 5, 8, 'O');
-            checkWinner (2, 5, 8, 'O');   
         
             checkWinner (0, 1, 2, 'X');
             checkWinner (3, 4, 5, 'X');
             checkWinner (6, 7, 8, 'X');
-            checkWinner (2, 1, 0, 'X');
-            checkWinner (5, 4, 3, 'X');
-            checkWinner (8, 7, 6, 'X');
             checkWinner (0, 4, 8, 'X');
-            checkWinner (8, 4, 0, 'X');
             checkWinner (2, 4, 6, 'X');
-            checkWinner (6, 4, 2, 'X');
             checkWinner (0, 3, 6, 'X');
-            checkWinner (6, 3, 0, 'X');
             checkWinner (1, 4, 7, 'X');
-            checkWinner (1, 4, 7, 'X');
-            checkWinner (2, 5, 8, 'X');
             checkWinner (2, 5, 8, 'X');
         })
     } 
